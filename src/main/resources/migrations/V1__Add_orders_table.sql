d4995c8c8db793d3a33f4f23eede2772ed7b64a7
Kristian Rekstad <kre@capraconsulting.no>
2023-03-09 15:34:40 +0100

    Add correct index
    Not enabled, can be found during testing

--- src/main/resources/migrations/V1__Add_orders_table.sql
+++ src/main/resources/migrations/V1__Add_orders_table.sql
@@ -7,5 +7,7 @@ CREATE TABLE orders
 
 -- Avoid multiple orders with the same ID
 CREATE UNIQUE INDEX ON orders ((data ->> 'orderId'));
+
 -- Search based on customer id
 CREATE INDEX order_customer_gin_index ON orders USING gin ((data -> 'customer' ->> 'id') jsonb_path_ops);
+--CREATE INDEX order_customer_index ON orders ((data -> 'customer' -> 'id'));
